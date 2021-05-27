effbc0dfe03334d27de9fad3de7ee8cdc0c0934c
jim <takurway@gmail.com>
2021-05-27 20:07:04 +0900

    Member menu

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='main'),
-
+    path('people/', views.people, name='main'),
+    path('research/', views.research, name='main'),
+    path('publication/', views.publication, name='main'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+def people(request):
+    return render(request, 'main/people.html')
+
+def research(request):
+    return render(request, 'main/research.html')
+
+def publication(request):
+    return render(request, 'main/publication.html')
