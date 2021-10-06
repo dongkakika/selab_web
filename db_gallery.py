ba4f953c3e7d8edc418919c0a0886edaa91fdff6
dongkaika <takurway@gmail.com>
2021-10-06 16:14:18 +0900

    version update

---
+++ db_gallery.py
@@ -0,0 +1,33 @@
+import sqlite3
+import codecs
+import os
+
+### Main ###
+con = sqlite3.connect('db.sqlite3')
+cur = con.cursor()
+
+file_names = os.listdir('media/image/default/')
+contents = [
+    'This is a test image.',
+    'This is a test gif image.',
+    'This is the central library in CBNU.',
+    'This is the main entrance image of CBNU.',
+    'Find where you want to go with this map !',
+    'The autumn sky of Chungju city.',
+    'The first picture of the jeju conference at the airport.',
+    'Our lab members had a conference and a great time in Jeju.',
+    'Our professor appeared in a local article.',
+    'This is our department building.',
+    '-',
+    
+    ]
+
+idx = 0
+for i in file_names:
+    li = [i[0:-4], "image/default/"+i, contents[idx]]
+    idx += 1
+
+    cur.execute('insert into gallery (title, img, content) values(?, ?, ?)', li)
+    con.commit()
+
+con.close()
---
+++ x2_all_migrations.cmd
@@ -0,0 +1,15 @@
+python manage.py makemigrations main
+python manage.py migrate
+
+python manage.py makemigrations notice
+python manage.py migrate
+
+python manage.py makemigrations people
+python manage.py migrate
+
+python manage.py makemigrations ppr
+python manage.py migrate
+
+python manage.py makemigrations tabs
+python manage.py migrate
+
